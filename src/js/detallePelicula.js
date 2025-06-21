0d4e7e7e4e16a5fed6f4ffe690822e830eb640b5
agustin-sequeira <agusseq@gmail.com>
2025-06-21 01:35:01 -0300

    agregue mas datos a json de peliculas
    monsters inc y civil war muestran sus datos de forma dinámica

--- src/js/detallePelicula.js
+++ src/js/detallePelicula.js
@@ -13,15 +13,25 @@ const url = new URL(window.location.href);
 const tituloPelicula = url.searchParams.get("titulo");
 
 document.getElementById("titulo-pelicula").textContent = tituloPelicula;
-
 const pelicula = peliculas.find(p => p.titulo === tituloPelicula);
-console.log(pelicula);
+
+//console.log(pelicula);
 
 class detallePelicula{
-    generarVista(peliculas){
-        for(let pelicula of peliculas){
+    generarVista(pelicula){
             document.getElementById("duracion-pelicula").textContent = pelicula.duracion;
             document.getElementById("genero-pelicula").textContent = pelicula.genero;
-        }
+            document.getElementById("actores-pelicula").textContent = pelicula.actores;
+            document.getElementById("descripcion-pelicula").textContent = pelicula.descripcion;
+            document.querySelector(".infoContent").innerHTML = pelicula.iframe;
+        
     }
 }
+
+if (pelicula) {
+    const vista = new detallePelicula();
+    vista.generarVista(pelicula);
+} else {
+    console.warn("Película no encontrada");
+}
+
--- src/json-data/peliculas.js
+++ src/json-data/peliculas.js
@@ -1,8 +1,107 @@
 const PELICULAS = [
-    {
-        "titulo": "Capitan America Civil War",
-        "duracion": "147 minutos",
-        "genero": "Acción, Ciencia Ficcion, Superheroes",
-        "actores": "Chris Evans, Robert Downey Jr, Scarlett Johansson"
-    }
+  {
+    "titulo": "Capitan America Civil War",
+    "duracion": "147 minutos",
+    "genero": "Acción, Ciencia Ficcion, Superheroes",
+    "actores": "Chris Evans, Robert Downey Jr, Scarlett Johansson",
+    "descripcion": `La lucha por la justicia divide a los Vengadores cuando un nuevo 
+acuerdo gubernamental enfrenta a Capitán América y Iron Man. 
+Lealtades se rompen y comienza una guerra entre héroes.`,
+    "iframe": `<iframe width="560" height="315" src="https://www.youtube.com/embed/dKrVegVI0Us?si=KF1mMDdkvg02pwHJ" 
+title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<a href="https://www.youtube.com/watch?v=dKrVegVI0Us&ab_channel=MarvelEntertainment" target="_blank">
+  <input type="button" value="Comenzar" class="infoButton">
+</a>`
+  },
+  {
+    "titulo": "Monsters Inc",
+    "duracion": "92 minutos",
+    "genero": "Animación, Comedia, Aventura",
+    "actores": "John Goodman, Billy Crystal",
+    "descripcion": `Monstruópolis es una ciudad habitada por monstruos que obtienen su energía asustando niños. 
+Pero todo cambia cuando una niña humana entra accidentalmente a su mundo, poniendo en riesgo su forma de vida y 
+revelando un secreto que cambiará todo.`,
+    "iframe": `<iframe width="560" height="315" src="https://www.youtube.com/embed/uzkkh-1A_kY?si=dsNBjN4p-0BOuFkk" 
+title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<a href="https://www.youtube.com/watch?v=uzkkh-1A_kY&ab_channel=XDP1026DX" target="_blank">
+  <input type="button" value="Comenzar" class="infoButton">
+</a>`
+  },
+  {
+    "titulo": "The Last Action Hero",
+    "duracion": "131 minutos",
+    "genero": "Acción, Fantasía, Comedia",
+    "actores": "Arnold Schwarzenegger, Austin O'Brien",
+    "descripcion": `Un chico consigue un ticket mágico que lo transporta dentro de su película de acción favorita, 
+donde héroe y realidad se mezclan y deben detener a un villano en el mundo real.`,
+    "iframe": `<iframe width="560" height="315" src="https://www.youtube.com/embed/ShBw43KJoLk" 
+title="Last Action Hero Trailer" frameborder="0" allowfullscreen></iframe>
+<a href="https://www.youtube.com/watch?v=ShBw43KJoLk" target="_blank">
+  <input type="button" value="Comenzar" class="infoButton">
+</a>`
+  },
+  {
+    "titulo": "Lightyear",
+    "duracion": "105 minutos",
+    "genero": "Animación, Acción, Aventura",
+    "actores": "Chris Evans, Keke Palmer",
+    "descripcion": `La historia del astronauta legendario Buzz Lightyear mientras lucha por rescatar a una tripulación 
+varada y enfrentarse al malvado emperador Zurg.`,
+    "iframe": `<iframe width="560" height="315" src="https://www.youtube.com/embed/BwZs3H_UN3k" 
+title="Lightyear Trailer" frameborder="0" allowfullscreen></iframe>
+<a href="https://www.youtube.com/watch?v=BwZs3H_UN3k" target="_blank">
+  <input type="button" value="Comenzar" class="infoButton">
+</a>`
+  },
+  {
+    "titulo": "Capitan America y el Soldado del Invierno",
+    "duracion": "136 minutos",
+    "genero": "Acción, Ciencia Ficción, Superhéroes",
+    "actores": "Chris Evans, Scarlett Johansson",
+    "descripcion": `Steve Rogers se enfrenta a una conspiración dentro de SHIELD mientras lucha contra el enigmático 
+Soldado del Invierno y descubre secretos oscuros.`,
+    "iframe": `<iframe width="560" height="315" src="https://www.youtube.com/embed/Zk1ELX5r5s0" 
+title="Winter Soldier Trailer" frameborder="0" allowfullscreen></iframe>
+<a href="https://www.youtube.com/watch?v=Zk1ELX5r5s0" target="_blank">
+  <input type="button" value="Comenzar" class="infoButton">
+</a>`
+  },
+  {
+    "titulo": "Capitan America el Primer Vengador",
+    "duracion": "124 minutos",
+    "genero": "Acción, Aventura, Fantasía",
+    "actores": "Chris Evans, Hayley Atwell",
+    "descripcion": `Durante la Segunda Guerra Mundial, Steve Rogers se convierte en el súper soldado Capitán América 
+para enfrentarse al villano Cráneo Rojo y detener HYDRA.`,
+    "iframe": `<iframe width="560" height="315" src="https://www.youtube.com/embed/JerVrbLldXw" 
+title="First Avenger Trailer" frameborder="0" allowfullscreen></iframe>
+<a href="https://www.youtube.com/watch?v=JerVrbLldXw" target="_blank">
+  <input type="button" value="Comenzar" class="infoButton">
+</a>`
+  },
+  {
+    "titulo": "Cars",
+    "duracion": "117 minutos",
+    "genero": "Animación, Comedia, Deportes",
+    "actores": "Owen Wilson, Paul Newman",
+    "descripcion": `Lightning McQueen, un auto de carreras egocéntrico, aprende sobre la amistad y lo importante que 
+es el camino cuando queda varado en Radiator Springs.`,
+    "iframe": `<iframe width="560" height="315" src="https://www.youtube.com/embed/SRXgG5B8fpg" 
+title="Cars Trailer" frameborder="0" allowfullscreen></iframe>
+<a href="https://www.youtube.com/watch?v=SRXgG5B8fpg" target="_blank">
+  <input type="button" value="Comenzar" class="infoButton">
+</a>`
+  },
+  {
+    "titulo": "La Gran Final",
+    "duracion": "57 minutos",
+    "genero": "Documental, Deportes",
+    "actores": "Lionel Messi, Emiliano Martínez",
+    "descripcion": `Documental de Disney+ que revive la emocionante final del Mundial Qatar 2022, 
+con testimonios de Messi y sus compañeros sobre esos momentos decisivos.`,
+    "iframe": null
+  }
 ];
+
