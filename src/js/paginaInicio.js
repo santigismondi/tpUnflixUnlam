d9de62405a9b437f0bfa7b9344ba8e5958855093
Santi Gismondi <129218589+santigismondi@users.noreply.github.com>
2025-06-25 14:57:02 -0300

    commit favoritos

--- src/js/paginaInicio.js
+++ src/js/paginaInicio.js
@@ -28,8 +28,7 @@ function recopilarContenido() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const contenidoRecopilado = recopilarContenido();
-    
+    const contenidoRecopilado = recopilarContenido();    
     window.listaPeliculas = contenidoRecopilado.peliculas;
     window.listaSeries = contenidoRecopilado.series;
     window.listaTodoElContenido = contenidoRecopilado.todoElContenido;
@@ -38,9 +37,10 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('DOMContentLoaded', () => {
     const selectCategorias = document.getElementById('listaCategorias');
     const inputBuscador = document.getElementById('buscador');
-    const grillaInicio = document.querySelector('.grillaInicio'); 
+    //const grillaInicio = document.querySelector('.grillaInicio'); 
     const boxContenidos = document.querySelectorAll('.boxContenido'); 
-
+    const botonCorazonFav = document.querySelectorAll('.fa-heart');
+    
     function aplicarFiltros() {
         const categoriaSeleccionada = selectCategorias.options[selectCategorias.selectedIndex].text.toLowerCase(); 
         const textoBusqueda = inputBuscador.value.toLowerCase().trim();
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
 
             if (mostrarPorCategoria && mostrarPorBusqueda) {
-                box.style.display = 'block';
+                box.style.display = 'flex';
             } else {
                 box.style.display = 'none';
             }
@@ -76,4 +76,62 @@ document.addEventListener('DOMContentLoaded', () => {
     inputBuscador.addEventListener('keyup', aplicarFiltros);
 
     aplicarFiltros();
+
+    botonCorazonFav.forEach(boton => {
+        const nombrePeliSerie = boton.getAttribute('data-titulo-item');
+        usuarioLogueadoJSON = sessionStorage.getItem('usuarioLogueado');            
+        const usuarioLogueado = JSON.parse(usuarioLogueadoJSON);
+        let usuariosJSON = localStorage.getItem('usuarios');
+        let usuarios = usuariosJSON ? JSON.parse(usuariosJSON) : [];
+        if (!usuarioLogueado.favoritos) {
+            usuarioLogueado.favoritos = [];
+        }
+        const usuarioIndex = usuarios.findIndex(u => u.nombreUsuario === usuarioLogueado.nombreUsuario); 
+        const peliculaSerieIndex = usuarioLogueado.favoritos.indexOf(nombrePeliSerie);
+
+        if (usuarioLogueado.favoritos[peliculaSerieIndex] === nombrePeliSerie) {
+            actualizarCorazonVisual(boton, true);
+        } else {
+            actualizarCorazonVisual(boton, false);
+        }
+        boton.addEventListener('click', () => {                
+            if (boton.classList.contains('fa-regular')) {
+                boton.classList.remove('fa-regular');
+                boton.classList.add('fa-solid');
+                usuarioLogueado.favoritos.push(nombrePeliSerie);
+                sessionStorage.setItem('usuarioLogueado', JSON.stringify(usuarioLogueado));
+                if (usuarioIndex !== -1) {
+                    usuarios[usuarioIndex] = usuarioLogueado; 
+                    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+                    actualizarCorazonVisual(boton, true);
+                    console.log("Array 'usuarios' general actualizado en localStorage.");
+                }
+                console.log(nombrePeliSerie);
+            } else {
+                boton.classList.remove('fa-solid');
+                boton.classList.add('fa-regular');
+                usuarioLogueado.favoritos.splice(peliculaSerieIndex, 1);
+                sessionStorage.setItem('usuarioLogueado', JSON.stringify(usuarioLogueado));
+                if (usuarioIndex !== -1) {
+                    usuarios[usuarioIndex] = usuarioLogueado; 
+                    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+                    console.log("Array 'usuarios' general actualizado en localStorage.");
+                }
+                actualizarCorazonVisual(boton, false);
+                console.log('Clase cambiada a fa-regular');
+            }
+
+        });
+    });
+
 });
+
+function actualizarCorazonVisual(btnCorazon, isFavorite) {
+    if (isFavorite) {
+        btnCorazon.classList.remove('fa-regular');
+        btnCorazon.classList.add('fa-solid');
+    } else {
+        btnCorazon.classList.remove('fa-solid');
+        btnCorazon.classList.add('fa-regular');
+    }
+}
